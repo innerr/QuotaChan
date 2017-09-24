d2df75ae6ee6d30ba5f5784a9ca5c9fa16f65bf7
zhexuany <yangzhexuan@pingcap.com>
2017-09-24 13:35:52 -0500

    reformat chan.h

--- chan.h
+++ chan.h
@@ -11,45 +11,44 @@ using std::mutex;
 using std::queue;
 using std::unique_lock;
 
-template <typename T>
-class Chan {
-    queue<T> que;
-    mutable mutex mtx;
-    condition_variable cond;
+template <typename T> class Chan {
+  queue<T> que;
+  mutable mutex mtx;
+  condition_variable cond;
 
-    const size_t quota;
-    size_t passed;
+  const size_t quota;
+  size_t passed;
 
 public:
-    Chan(size_t quota_) : quota(quota_), passed(0) {}
+  Chan(size_t quota_) : quota(quota_), passed(0) {}
 
-    inline void Push(const T &v) {
-        {
-            unique_lock<mutex> lock{mtx};
-            que.push(v);
-        }
-        cond.notify_one();
+  inline void Push(const T &v) {
+    {
+      unique_lock<mutex> lock{mtx};
+      que.push(v);
     }
+    cond.notify_one();
+  }
 
-    inline bool Pop(T &v) {
-        unique_lock<mutex> lock{mtx};
-        if (passed == quota)
-            return false;
+  inline bool Pop(T &v) {
+    unique_lock<mutex> lock{mtx};
+    if (passed == quota)
+      return false;
 
-        while (que.empty() && passed < quota)
-            cond.wait(lock);
+    while (que.empty() && passed < quota)
+      cond.wait(lock);
 
-        if (que.empty())
-            return false;
+    if (que.empty())
+      return false;
 
-        v = que.front();
-        que.pop();
-        ++passed;
+    v = que.front();
+    que.pop();
+    ++passed;
 
-        if (passed == quota)
-            cond.notify_all();
-        return true;
-    }
+    if (passed == quota)
+      cond.notify_all();
+    return true;
+  }
 };
 
-}
+} // namespace ptio
